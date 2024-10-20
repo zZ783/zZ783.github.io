0ae1edabcf9b4a004b583c05e54556800597f137
zZ783 <783497190@qq.com>
2024-10-20 13:35:37 +0800

    Site updated: 2024-10-20 13:35:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/17/hello-world/"];function toRandomPost(){
+var posts=["2024/10/17/hello-world/","2024/10/20/C/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
